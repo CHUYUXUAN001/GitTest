8c2642adf7f3d45a150e951168996f93f2d0b1a1
cyx <635732897@qq.com>
2022-06-13 11:04:23 +0800

    hot-fix commit1

--- src/main/java/com/bjpowernode/TheOne.java
+++ src/main/java/com/bjpowernode/TheOne.java
@@ -3,6 +3,6 @@ package com.bjpowernode;
 public class TheOne {
     public static void main(String[] args) {
         System.out.println(2+3);
-        System.out.println("master");
+        System.out.println("hot-fix");
     }
 }
