b336db9d646558b797efdf6211964b75efef79f8
CHUYUXUAN001 <101853212+CHUYUXUAN001@users.noreply.github.com>
2022-06-13 15:10:59 +0800

    pull commit

--- src/main/java/com/bjpowernode/TheOne.java
+++ src/main/java/com/bjpowernode/TheOne.java
@@ -7,5 +7,6 @@ public class TheOne {
         System.out.println(2+3);
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
