bed0c91306c596b7740a3078da882435d52a236f
cyx <635732897@qq.com>
2022-06-13 14:56:31 +0800

    pull commit

--- src/main/java/com/bjpowernode/TheOne.java
+++ src/main/java/com/bjpowernode/TheOne.java
@@ -1,8 +1,11 @@
 package com.bjpowernode;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class TheOne {
     public static void main(String[] args) {
         System.out.println(2+3);
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
